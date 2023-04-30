b534d778d09c0abb9fe9f665d622e8caa00d5f1c
olivia meyer <olivia73@gmail.com>
2023-04-30 18:04:38 +0200

    check user input

--- game.py
+++ game.py
@@ -5,23 +5,33 @@ user_guess = false
 attempts = 0 
 
 # Game loop
-
+while user_guess := True
     # Get user input
-
+    answer = input ("enter a number btw 1 and 100")
+   
+   
     # Check the user input
-    user_guess = True
+    try:
+        answer_number = int (answer)
+    except:
+            print("Error: please enter a valid number")
+            quit
 
-    if user_guess:
-    print("The answer is correct!")
-
-    number = input("Type a number: ")
+   
 
 
 
     # Increase attempt count
 
-    # Check the user answer againt the secret number
+    attempts += 1
 
+    # Check the user answer againt the secret number
+ user_guess = True
+
+    if user_guess:
+    print("The answer is correct!")
+
+    number = input("Type a number: ")
 # Get the spelling of the "attempt" word
 
 # Display the result

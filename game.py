5e7d8d3aedc70efa74ff53e2377845d4cc7ea5e4
olivia meyer <olivia73@gmail.com>
2023-04-30 17:56:12 +0200

    added set up

---
+++ game.py
@@ -0,0 +1,31 @@
+# Setup
+import random
+random_number = random.randint(1, 100)
+user_guess = false
+attempts = 0 
+
+# Game loop
+
+    # Get user input
+
+    # Check the user input
+    user_guess = True
+
+    if user_guess:
+    print("The answer is correct!")
+
+    number = input("Type a number: ")
+
+
+
+    # Increase attempt count
+
+    # Check the user answer againt the secret number
+
+# Get the spelling of the "attempt" word
+
+# Display the result
+
+
+
+
